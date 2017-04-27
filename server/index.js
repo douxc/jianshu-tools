701f38de8f6367bedf75ec751ed540a842e5e1c1
金伟强 <iamjoel007@gmail.com>
2017-04-27 20:23:50 +0800

    finish fetch ariticle and collection

--- server/index.js
+++ server/index.js
@@ -6,11 +6,7 @@ const loggeredCoodie = 'xxx=xx;ddd=fdfd;'
   // 获取消息通知
 var fetchNotifications = (loginedCookieValue) => {
   var url = `${JIANSHU_SERVER}/notifications?type=follows&page=1`
-  var j = request.jar();
-  loginedCookieValue.split(';').forEach(item => {
-    var cookie = request.cookie(item)
-    j.setCookie(cookie, url)
-  })
+  var j = generatorLoginCookieJar(loginedCookieValue, url)
   request({
     url: url,
     jar: j,
@@ -19,13 +15,91 @@ var fetchNotifications = (loginedCookieValue) => {
     }
   }, function (error, response, body) {
     console.log(body)
-    // console.log('---------------')
-    // console.log(response)
-
   })
+}
+http://www.jianshu.com/u/EhUmA3?order_by=shared_at&_pjax=%23list-container
 
+// 获取文章列表 TODO 有没有接口 获得用户id EhUmA3
+var fetchArticles = (loginedCookieValue) => {
+  var url = `${JIANSHU_SERVER}/u/EhUmA3?order_by=shared_at`
+  var j = generatorLoginCookieJar(loginedCookieValue, url)
+  request({
+    url: url,
+    jar: j,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }, function (error, response, body) {
+    var neededInfo = ['id', 'title', 'shared_at']
+    var note = JSON.parse(body).notes.map(article => {
+      var res = {}
+      neededInfo.forEach(key => {
+        res[key] = article[key]
+      })
+      return res
+    })
+    console.log(note)
+  })
 }
 
-// function 
+// 获取专题
+var fetchCollections = (loginedCookieValue, q) => {
+  var url = `${JIANSHU_SERVER}/search/do?q=${encodeURIComponent(q)}&type=collection&page=1&order_by=top`
+  var j = generatorLoginCookieJar(loginedCookieValue, url)
+  request({
+    url: url,
+    jar: j,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }, function (error, response, body) {
+    var neededInfo = ['id', 'title', 'public_notes_count','likes_count']
+    var collection = JSON.parse(body).entries.map(article => {
+      var res = {}
+      neededInfo.forEach(key => {
+        res[key] = article[key]
+      })
+      return res
+    })
+    console.log(collection)
+  })
+}
 
-fetchNotifications(loggeredCoodie)
+// 投稿
+var submitArticle = (loginedCookieValue, articleId, collectionId) => {
+  var url = `${JIANSHU_SERVER}/notes/${articleId}/submit`
+  var j = generatorLoginCookieJar(loginedCookieValue, url)
+  request({
+    url: url,
+    jar: j,
+    method: 'POST',
+    json: true,
+    body: {
+      collection_id: collectionId
+    },
+    headers: {
+      'Accept': 'application/json'
+    }
+  }, function (error, response, body) {
+    console.log(body)
+  })
+}
+
+function generatorLoginCookieJar(loginedCookieValue, url) {
+  var j = request.jar();
+  loginedCookieValue.split(';').forEach(item => {
+    var cookie = request.cookie(item)
+    j.setCookie(cookie, url)
+  })
+  return j
+}
+
+// fetchNotifications(loggeredCoodie)
+// fetchArticles(loggeredCoodie)
+// fetchCollections(loggeredCoodie, '前端')
+// submitArticle(loggeredCoodie, 11819604, 937)
+
+/*
+ 专栏 id 1084 Web 前端之路
+ 文章 11819604 vue-router2 写法示例
+*/
